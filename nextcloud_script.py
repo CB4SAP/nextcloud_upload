4c8bd4149654b6b18a307f66c888665065af02dd
cb4lenor <cb4lenor@gmx.de>
2023-05-05 09:35:20 +0200

    first

---
+++ nextcloud_script.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+def check_file_exists(url, username=None, password=None):
+    response = requests.request("PROPFIND", url, auth=(username, password))
+    return response.status_code == 200
+
+def initialization(user="", password="", nextcloudAdress="",folder=""):
+    
+    if user =="":
+        user = input("User: ")
+    if password =="":
+        password = input("password: ")
+    if nextcloudAdress =="":
+        nextcloudAdress = input("nextcloud address: ")
+    if folder =="":
+        folder = input("subfolder: ")
+    
+    returnItems = {}
+    returnItems["user"] = user
+    returnItems["password"] = password
+    returnItems["nextcloudAddress"] = nextcloudAdress
+    returnItems["subfolder"] = folder
+    return returnItems
+
+
+
+def processing(config):
+    dir = os.listdir()
+    for i in dir:
+        url = f'https://{config["nextcloudAddress"]}/remote.php/dav/files/{config["user"]}/{config["subfolder"]}/{i}'
+        if check_file_exists(url, config["user"], config["password"]) == False:
+            if os.path.isfile(i):
+                file=open(i, 'rb')
+                r = requests.put(url, data=file.read(),auth = HTTPBasicAuth(config["user"], config["password"]))
+                if r.status_code != 201:
+                    print(f"error uploading file: {i} to {url}")
+                    print(f"error message: {r}")
+            else:
+                #create subfolder structure
+                r = requests.request("MKCOL",url, auth = HTTPBasicAuth(config["user"], config["password"]))
+                
+                lastDirectory=os.getcwd()
+                newDirectory = f"{lastDirectory}/{i}"
+                os.chdir(newDirectory)
+            
+                newConfig = config.copy()
+                newConfig["subfolder"] = f'{config["subfolder"]}/{i}/'
+                
+                processing(newConfig)
+                
+                os.chdir(lastDirectory)
+#       result = os.system(f"curl -g -u {user}:{password} -T '{i}' https://{nextcloudAdress}/remote.php/dav/files/{user}/{folder}/")
+
+
+config = initialization()
+print(config)
+processing(config)
